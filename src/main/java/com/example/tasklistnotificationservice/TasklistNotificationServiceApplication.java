77d5561449e75e1bc2372ef61e0dbfbf0a8fceac
Nikita <nikitamasslennikov@mail.ru>
2023-09-24 14:00:45 +0300

    Initial commit

---
+++ src/main/java/com/example/tasklistnotificationservice/TasklistNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasklistnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklistNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklistNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tasklistnotificationservice/TasklistNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasklistnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasklistNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
